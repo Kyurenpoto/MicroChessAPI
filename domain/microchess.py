9f36795b2e01471a619ca8df481684956997a267
Kyurenpoto <heal9179@gmail.com>
2021-04-30 04:47:50 +0900

    Extract domain layer

---
+++ domain/microchess.py
@@ -0,0 +1,51 @@
+# SPDX-FileCopyrightText: © 2021 Kyurenpoto <heal9179@gmail.com>
+
+# SPDX-License-Identifier: GPL-3.0-only
+
+from abc import ABCMeta, abstractmethod
+from typing import List
+from enum import Enum
+
+import chess
+
+class MicroFen:
+    __slots__ = ["__fen"]
+
+    def __init__(self, fen: str):
+        self.__fen = fen
+
+    def __str__(self):
+        return self.__fen
+
+class MicroMove:
+    def __init__(self, move: str):
+        pass
+
+class MicroBoardStatus(Enum):
+    NONE = 0
+    CHECKMATE = 1
+    STALEMATE = 2
+
+class MicroBoardBase(metaclass=ABCMeta):
+    @abstractmethod
+    def fen(self) -> MicroFen:
+        pass
+
+    @abstractmethod
+    def move(self, move: MicroMove):
+        pass
+
+class MicroBoard(MicroBoardBase):
+    def __init__(self, fen: MicroFen):
+        pass
+
+    def __init__(self, fen: str):
+        self.__init__(MicroFen(fen))
+
+    def fen(self) -> MicroFen:
+        pass
+
+    def move(self, move: MicroMove) -> MicroBoardBase:
+        pass
+
+boards: List[MicroBoard]
--- main.py
+++ main.py
@@ -2,72 +2,32 @@
 
 # SPDX-License-Identifier: GPL-3.0-only
 
-from abc import ABCMeta, abstractmethod
 from typing import List, Dict
-from enum import Enum
-import chess
+
 from fastapi import FastAPI, status
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 
-class MicroFen:
-    __slots__ = ["__fen"]
-
-    def __init__(self, fen: str):
-        self.__fen = fen
-
-    def __str__(self):
-        return self.__fen
-
-class MicroMove:
-    def __init__(self, move: str):
-        pass
-
-class MicroBoardStatus(Enum):
-    NONE = 0
-    CHECKMATE = 1
-    STALEMATE = 2
-
-class MicroBoardBase(metaclass=ABCMeta):
-    @abstractmethod
-    def fen(self) -> MicroFen:
-        pass
-
-    @abstractmethod
-    def move(self, move: MicroMove):
-        pass
-
-class MicroBoard(MicroBoardBase):
-    def __init__(self, fen: MicroFen):
-        pass
-
-    def __init__(self, fen: str):
-        self.__init__(MicroFen(fen))
-
-    def fen(self) -> MicroFen:
-        pass
-
-    def move(self, move: MicroMove) -> MicroBoardBase:
-        pass
-
-boards: List[MicroBoard]
+from domain import microchess
 
 app: FastAPI = FastAPI()
 
 class ActionTest(BaseModel):
     fen: str
     move: str
-
 class ActionTestResult(BaseModel):
     fen: str
-    status: MicroBoardStatus
+    status: microchess.MicroBoardStatus
     next_move_list: List[str]
 
+async def move_on_test(action: ActionTest) -> Dict:
+    result: ActionTestResult = ActionTestResult(fen="", status=microchess.MicroBoardStatus.NONE, next_move_list=[])
+    return jsonable_encoder(result)
+
 @app.put("/action-test", status_code=status.HTTP_200_OK)
-async def move_on_test(action: ActionTest) -> JSONResponse:
-    result: ActionTestResult = ActionTestResult(fen="", status=MicroBoardStatus.NONE, next_move_list=[])
-    return JSONResponse(content=jsonable_encoder(result))
+async def action_test(action: ActionTest) -> JSONResponse:
+    return JSONResponse(content=await move_on_test(action))
 
 class ActionTrain(BaseModel):
     fens: str
@@ -75,24 +35,33 @@ class ActionTrain(BaseModel):
 
 class ActionTrainResult(BaseModel):
     fens: List[str]
-    statuses: List[MicroBoardStatus]
+    statuses: List[microchess.MicroBoardStatus]
     next_move_lists: List[List[str]]
 
-@app.put("/action-train", status_code=status.HTTP_200_OK)
-async def move_on_train(action: ActionTrain) -> JSONResponse:
+async def move_on_train(action: ActionTrain) -> Dict:
     result: ActionTrainResult = ActionTrainResult(fens=[], statuses=[], next_move_lists=[])
-    return JSONResponse(content=jsonable_encoder(result))
+    return jsonable_encoder(result)
+
+@app.put("/action-train", status_code=status.HTTP_200_OK)
+async def action_train(action: ActionTrain) -> JSONResponse:
+    return JSONResponse(content=await move_on_train(action))
 
 class StateTest(BaseModel):
     fen: str
 
-@app.post("/state-test", status_code=status.HTTP_201_CREATED)
 async def reset_on_test(state: StateTest) -> Dict[str, bool]:
     return { "success": True }
+
+@app.post("/state-test", status_code=status.HTTP_201_CREATED)
+async def state_test(state: StateTest) -> Dict[str, bool]:
+    return await reset_on_test(state)
 
 class StateTrain(BaseModel):
     fens: str
 
-@app.post("/state-train", status_code=status.HTTP_201_CREATED)
 async def reset_on_train(state: StateTrain) -> Dict[str, bool]:
     return { "success": True }
+
+@app.post("/state-train", status_code=status.HTTP_201_CREATED)
+async def state_train(state: StateTrain) -> Dict[str, bool]:
+    return await reset_on_train(state)
