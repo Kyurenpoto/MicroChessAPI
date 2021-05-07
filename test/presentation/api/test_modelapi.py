d28b32c1a3daea57368d4cddf83df9b66637cc48
Kyurenpoto <heal9179@gmail.com>
2021-05-08 08:02:35 +0900

    Extract fixture from test for api

--- test/presentation/api/test_modelapi.py
+++ test/presentation/api/test_modelapi.py
@@ -2,6 +2,8 @@
 
 # SPDX-License-Identifier: GPL-3.0-only
 
+from typing import Dict, List
+
 import pytest
 from httpx import AsyncClient
 from fastapi import status
@@ -9,19 +11,46 @@ from fastapi import status
 from domain.implementation.microsan import MICRO_FIRST_MOVE_SAN
 from domain.implementation.microboard import MICRO_STARTING_FEN, MICRO_FIRST_MOVE_FEN
 from domain.implementation.legalsan import MICRO_FIRST_LEGAL_MOVES
-from main import app
+
 
 @pytest.mark.asyncio
-async def test_model_act() -> None:
-    client = AsyncClient(app=app, base_url="http://test")
-    response = await client.put(
+async def test_model_act_200(async_client: AsyncClient) -> None:
+    response = await async_client.put(
         url="/model/act",
-        json={"fens": [MICRO_STARTING_FEN],
-        "sans": [MICRO_FIRST_MOVE_SAN]})
-    await client.aclose()
+        json={"fens": [MICRO_STARTING_FEN], "sans": [MICRO_FIRST_MOVE_SAN]},
+    )
 
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == {
         "fens": [MICRO_FIRST_MOVE_FEN],
         "statuses": [0],
-        "legal_moves": [MICRO_FIRST_LEGAL_MOVES]}
+        "legal_moves": [MICRO_FIRST_LEGAL_MOVES],
+    }
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "json",
+    [
+        (
+            {
+                "fens": [MICRO_STARTING_FEN, MICRO_STARTING_FEN],
+                "sans": [MICRO_FIRST_MOVE_SAN],
+            }
+        ),
+        (
+            {
+                "fens": [MICRO_STARTING_FEN],
+                "sans": [MICRO_FIRST_MOVE_SAN, MICRO_FIRST_MOVE_SAN],
+            }
+        ),
+        ({"fens": [MICRO_STARTING_FEN], "sans": []}),
+        ({"fens": [], "sans": [MICRO_FIRST_MOVE_SAN]}),
+    ],
+)
+async def test_model_act_400(
+    async_client: AsyncClient, json: Dict[str, List], msg: str
+) -> None:
+    response = await async_client.put(url="/model/act", json=json)
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
